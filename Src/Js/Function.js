1cb34ff230e2c43e9372da41b9972814e379680f
DylanMC14 <dmadrigalc@fwdcostarica.com>
2023-07-19 16:00:28 -0600

    "Actalizacion 3 del proyecto (Menu)"

--- Src/Js/Function.js
+++ Src/Js/Function.js
@@ -2,9 +2,9 @@ const playSound = function() {
     let element = document.createElement('div');
     element.setAttribute('style', 'display: none');
     element.innerHTML = `
-      <audio controls autoplay>
-      <source src="Src/Audio/Lost Frequencies - Are You With Me (Official Music Video).mp3" type="audio/mp3">
-  </audio>
+    <audio autoplay>
+    <source src="Src/Audio/lost frequencies ft. sandro cavazza - beautiful life (sub español).mp3" type="audio/mp3">
+ </audio>
     `;
     document.body.appendChild(element);
     document.removeEventListener('click', playSound);
