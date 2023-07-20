b389d3adcd4364c9e06f445af0d079329c09b74e
JoseDJarias <arias_da044@hotmail.com>
2023-07-20 09:55:32 -0600

    "curri 3"

--- Src/Js/curriculum3.js
+++ Src/Js/curriculum3.js
@@ -10,29 +10,20 @@ const percentageDecimal = percentage / 100; //0.5
 
 // Calcular cuántos círculos deben estar llenos en función del porcentaje
 const totalCircles = circles.length; //6
-const filledCirclesCount = Math.round(percentageDecimal * totalCircles); //3
+
+// const filledCirclesCount = Math.round(percentageDecimal * totalCircles); //3
 //const filledCirclesCount = Math.round((percentage / 100) * circles.length);
 // Recorrer todos los círculos
 
 
 //[oooooo]
 
-for (let i = 0; i < circles.length; i++) {
-    // Si el índice (i) es menor que el número de círculos llenos, agregar la clase 'filled'
-    if (i < filledCirclesCount) {
-        circles[i].classList.add('filled');
-    } else {
-        // Si el índice (i) es mayor o igual al número de círculos llenos, quitar la clase 'filled'
-        circles[i].classList.remove('filled');
-    }
-}
-
 function addCircle(percentageDecimal,totalCircles) {
     
-    const f = Math.round(percentageDecimal * totalCircles); //3
-    for (let i = 0; i < circles.length; i++) {
+    const filledCirclesCount = Math.round(percentageDecimal * totalCircles); //3
+    for (let i = 0; i < totalCircles; i++) {
         // Si el índice (i) es menor que el número de círculos llenos, agregar la clase 'filled'
-        if (i < f) {
+        if (i < filledCirclesCount) {
             circles[i].classList.add('filled');
         } else {
             // Si el índice (i) es mayor o igual al número de círculos llenos, quitar la clase 'filled'
@@ -41,16 +32,34 @@ function addCircle(percentageDecimal,totalCircles) {
     }
 }
 
+highPorcentaje= 85/100
 mediumPorcentaje = 50 /100
 lowPorcentaje = 30/100
-highPorcentaje= 85/100
 
-addCircle(lowPorcentaje,totalCircles);
-
+addCircle(mediumPorcentaje,totalCircles);
 
 
+// for (let i = 0; i < totalCircles; i++) {
+//     // Si el índice (i) es menor que el número de círculos llenos, agregar la clase 'filled'
+//     if (i < filledCirclesCount) {
+//         circles[i].classList.add('filled');
+//     } else {
+//         // Si el índice (i) es mayor o igual al número de círculos llenos, quitar la clase 'filled'
+//         circles[i].classList.remove('filled');
+//     }
+// }
 
 
+
+
+
+// addChild(){
+//     const progressBar =document.querySelector('progress-bar' );
+//     const createCircle = document.createElement("p");
+
+//     createCircle.
+//  progressBar.appen()
+// }
 
 
 
